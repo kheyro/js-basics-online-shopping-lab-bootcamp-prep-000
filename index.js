a9d1d3bb02b74fb3a45f38a8d7b7235286c9457c
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 10:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ function total() {
   var total = 0;
   for(let i = 0;i < cart.length; i++) {
     for (var key in cart[i]) {
-      console.log(cart[i][key]);
       total += parseFloat(cart[i][key]);
     }
   }
+  console.log(total);
   return total;
 }
 

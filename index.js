db3f4cb9be8f9bcafc23dde1ebfe3a49b39394aa
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 08:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(itemName) {
 
 function viewCart() {
   // write your code here
-  if (!cart.length) {
+  if (!getCart.length) {
     return "Your shopping cart is empty";
   }
 

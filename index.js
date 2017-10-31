00b409c47550dd0fc726f18805948506b323787a
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 10:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,9 +69,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  let msg;
   if(cardNumber) {
+    msg = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart = [];
-    return console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    return console.log(msg);
   } else {
     return console.log("Sorry, we don't have a credit card on file for you.");
   }

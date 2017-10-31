b32e15ae231da90a91852625c1c1f5567d0228a9
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 09:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,36 +32,36 @@ function viewCart() {
   // return items.join(',');
 }
 
-function total() {
-  // write your code here
-  let total = 0;
-  for(let i = 0;i < cart.length; i++) {
-    for (var key in cart[i]) {
-      total += cart[i][key];
-    }
-  }
-  return total;
-}
-
-function removeFromCart(item) {
-  // write your code here
-  for(let i = 0;i < cart.length; i++) {
-    if(cart[i].hasOwnProperty(item)) {
-      cart.splice(i, 1);
-      return cart;
-    }
-  }
-
-  return "That item is not in your cart.";
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-  if(cardNumber) {
-    cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-  } else {
-    return "Sorry, we don't have a credit card on file for you.";
-  }
-
-}
+// function total() {
+//   // write your code here
+//   let total = 0;
+//   for(let i = 0;i < cart.length; i++) {
+//     for (var key in cart[i]) {
+//       total += cart[i][key];
+//     }
+//   }
+//   return total;
+// }
+// 
+// function removeFromCart(item) {
+//   // write your code here
+//   for(let i = 0;i < cart.length; i++) {
+//     if(cart[i].hasOwnProperty(item)) {
+//       cart.splice(i, 1);
+//       return cart;
+//     }
+//   }
+// 
+//   return "That item is not in your cart.";
+// }
+// 
+// function placeOrder(cardNumber) {
+//   // write your code here
+//   if(cardNumber) {
+//     cart = [];
+//     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+//   } else {
+//     return "Sorry, we don't have a credit card on file for you.";
+//   }
+// 
+// }

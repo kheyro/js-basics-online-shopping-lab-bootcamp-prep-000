e56642e9e80fd99c4472ea8f991d9764486f846c
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 08:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(itemName) {
  // write your code here
  var itemPrice = Math.floor(Math.random() * 100);
- cart.push({itemName: itemPrice});
+ cart.push({[itemName]: itemPrice});
  console.log(`${itemName} has been added to your cart.`);
  return cart;
 }

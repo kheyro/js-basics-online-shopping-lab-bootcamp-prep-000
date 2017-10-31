32f7f143d5dd9d98e241958a17e1fbb69e78aced
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 09:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(!cardNumber) {
+  if(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart = [];

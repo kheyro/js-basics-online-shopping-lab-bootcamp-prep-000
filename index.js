f46bda166344d2647b448861bfc9e61a73716d10
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 09:20:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   const items = [];
 
   for(let i = 0; i < cart.length; i++) {
-    items[i] = "you have ${cart[i].itemName} at $${itemPrice}";
+    items[i] = `you have ${cart[i].itemName} at $${itemPrice}`;
   }
 
   return items.join(',');

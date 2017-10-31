ce8f47235f910c4d358139c950d1332ad50b987e
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 08:54:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,19 +41,12 @@ function removeFromCart(item) {
   for(let i = 0;i < cart.length; i++) {
     if(cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
-      console.log(cart);
       return cart;
     }
   }
 
   return "That item is not in your cart.";
 }
-
-addToCart("vanilla");
-addToCart("watermelon");
-addToCart("yams");
-
-removeFromCart("watermelon");
 
 function placeOrder(cardNumber) {
   // write your code here
